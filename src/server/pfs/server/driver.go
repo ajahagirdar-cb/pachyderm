b5fbb8895b170c4b58531b15fac0c4869e9b1efd
Matthew Steffen <msteffen@pachyderm.io>
2019-05-14 16:30:43 -0700

    Add pipeline/job tracing

--- src/server/pfs/server/driver.go
+++ src/server/pfs/server/driver.go
@@ -1460,6 +1460,8 @@ nextSubvBranch:
 			return err
 		}
 	}
+	// associate new commits above with any extended traces that are running
+	attachNewCommitsToAnyExtendedTrace(d.etcdClient, d.prefix, stm)
 	return nil
 }
 
